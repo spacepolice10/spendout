1195ff6f1b907d74ad509c7e790a78aedafe127b
spacepolice10 <naysayer@hey.com>
2026-08-22 23:50:20 +0700

    Refactor form button styles in allocations, expenses, and sources to include larger button sizes for improved visibility and user experience. Cleaned up JavaScript target definitions for consistency.

--- app/javascript/controllers/form_details_controller.js
+++ app/javascript/controllers/form_details_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "summary", "summaryContent" ]
+  static targets = ["summary", "summaryContent"]
 
   connect() {
     this.abortController = new AbortController();
